098ea54a8d378b706ba0971b3f71e9155e324f9a
Madman-dev <dongjunlee222@gmail.com>
2024-07-02 18:25:47 +0900

    "[DOCS] : 📚 초기 프로젝트 셋업"

---
+++ ziririt-iOS/ziririt-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ziririt-iOS
+//
+//  Created by Porori on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
