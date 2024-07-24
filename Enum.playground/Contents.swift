c9a89dcc1e874f18986f23c9015bbbf4c1ef0662
Madman-dev <dongjunlee222@gmail.com>
2024-07-25 07:44:46 +0900

    [FEAT]: UIFont 스타일 적용

---
+++ Enum.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+enum Platforms {
+    case youtube
+    case twitch
+    case chizizik
+    case twitter
+}
+
+let testingPlatform = Platforms.twitch
+
+switch testingPlatform {
+case .twitch:
+case .chizizik:
+}
